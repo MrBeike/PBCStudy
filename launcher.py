d524560f65811afd67c115808ff1ecfd38a5bff4
xeroxYor <lbbas@126.com>
2020-05-07 09:02:09 +0800

    beautify display

--- launcher.py
+++ launcher.py
@@ -5,16 +5,16 @@ from PBCSTU import PBCSTU
 def workFlow(pbcstu):
     program_info = pbcstu.getProgram()
     for index,item in enumerate(program_info['datas']):
-        print(f"{index+1}:{item['name']}")
+        print(f"【{index+1}】:【{item['name']}】")
     index = int(input('请输入要学习的项目序号： '))
     project_info = pbcstu.getProject(program_info,index)
     for index,item in enumerate(project_info['datas'][0]['tasks']):
         status = ['NoStart','Studying','Completed']
-        print(f"{index+1}:{status[item['status']]}:{item['name']}")
+        print(f"【{index+1}】:【{status[item['status']]}】-->{item['name']}")
     index = int(input('请输入要学习的章节序号： '))
     chapter_info = pbcstu.getChapter(project_info,index)
     for index,item in enumerate(chapter_info['datas']):
-        print(f"{index+1}:{item['status']}:{item['title']}")
+        print(f"【{index+1}】:【{item['status']}】-->{item['title']}")
     index = int(input('请输入要学习的课程序号： '))
     knowledge_info =pbcstu.getKnowledge(chapter_info,index)
     print(f'知识点学习结果：{knowledge_info}')
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     pbcstu = PBCSTU()
     warning = input('本脚本仅供python学习研究使用，请勿用于其他非法用途：我同意[Y],我拒绝[N]')
     if warning.upper() == 'Y':
-        userName = input('请输入用户名（支持短号和身份证号码）： ')
-        password = input('请输入密码： ')
+        userName = input('请输入用户名（支持短号和身份证号码）： ').strip()
+        password = input('请输入密码： ').strip()
         user_info = pbcstu.login(userName,password)
         if user_info:
             print(f"您正在以{user_info['departmentName']}【{user_info['fullName']}】的身份登录。")
